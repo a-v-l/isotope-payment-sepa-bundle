6ab04182ff4970ef8f2852044287f0fbf23efc5d
Arndt von Lucadou <info@lucadou.net>
2023-02-07 16:18:36 +0100

    Initial commit

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @package   isotope-payment-sepa-bundle
+ * @author    Michael Gruschwitz <info@grusch-it.de>
+ * @license   LGPL-3.0+
+ * @copyright Michael Gruschwitz 2019
+ */
+
+namespace Gruschit\Contao\Isotope\Payment\Sepa\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Gruschit\Contao\Isotope\Payment\Sepa\GruschitContaoIsotopePaymentSepa;
+
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(GruschitContaoIsotopePaymentSepa::class)
+                        ->setLoadAfter([ContaoCoreBundle::class,'isotope'])
+                        ->setReplace([ 'isotope-sepa' ]),
+        ];
+    }
+}
